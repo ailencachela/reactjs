6643a81afe95d602c93c1545b35adc4b4463fce0
Leonardo <ailen.cachela@gmail.com>
2022-09-21 22:03:58 -0300

    a

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,7 +5,7 @@ export default function AboutUs() {
   return (
     <>
       <Typography style={{ margin: "32px" }}>
-        Bienvenidos a nuestra tienda. Esta página está en construcción
+        Bienvenidos a nuestra tienda. Esta página está en construcción.
       </Typography>
     </>
   );
